be7b7cee2ed7ef989ecacaaaeb3f1a2a03ccfa91
Tyler Richard <tylerrichard215@gmail.com>
2023-12-03 14:00:29 -0600

    Day 2 + 3

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,180 +1,86 @@
-use std::arch::asm;
+use std::cmp::max;
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{digit1, newline},
+    combinator::{map, map_res},
+    multi::separated_list0,
+    sequence::{preceded, terminated, tuple},
+    Err,
+};
 
 fn main() {
-    let input = include_str!("input.txt");
+    let input = include_str!("sample.txt");
 
-    assembly1(input);
     part1(input);
-    assembly2(input);
     part2(input);
 }
 
-fn assembly1(input: &'static str) {
-    let sum: u64;
-    const WIN_TABLE: [u8; 3] = [3, 0, 6];
+fn part1(input: &str) {
+    let maxes = (13, 14, 12);
+    let mut sum = 0;
 
-    unsafe {
-        asm! {
-            "mov {sum}, 0",
-            // Setup length
-            "shr {len}, 2",
-            "2:",
-            // Store 4 chars in {str}
-            "mov {arr:e}, [{str} + {len} * 4 - 4]",
-            // Mask space and newline
-            "sub {arr}, 0x00580041",
-            "mov {tmp}, 0x0000000000FF00FF",
-            "and {arr}, {tmp}",
-            // Calculate offset of comp move
-            // and player move. [-2, -1, 0, 1, 2]
-            "mov {tmp}, 0",
-            "mov {tmp:l}, {arr:l}",
-            "shr {arr}, 16",
-            "sub {tmp}, {arr}",
-            // Make everything positive
-            // -2 => 1, -1 => 2
-            "jns 3f",
-            "add {tmp}, 3",
-            "3:",
-            // Use win table to add bonus
-            "mov {tmp:l}, [{win} + {tmp}]",
-            "and {tmp}, 0xFF",
-            "add {sum}, {tmp}",
-            // Add move
-            "mov {tmp:l}, {arr:l}",
-            "and {tmp}, 0xFF",
-            "add {sum}, {tmp}",
-            "inc {sum}",
-            // Loop
-            "dec {len}",
-            "jnz 2b",
-            // Holds the pointer to the string
-            str = inout(reg) input as *const str as *const u8 => _,
-            // Holds the 4 working characters of the string
-            arr = out(reg) _,
-            // Temp
-            tmp = out(reg) _,
-            // Win table for +3 +0 or +6
-            win = in(reg) &WIN_TABLE,
-            // The length of the string, also the index in the loop
-            len = inout(reg) input.len() => _,
-            // The total sum
-            sum = out(reg) sum,
-        };
-    };
+    for (id, rounds) in parse(input).unwrap() {
+        if rounds.into_iter().fold(true, |res, (a, b, c)| {
+            res && (a <= maxes.0 && b <= maxes.1 && c <= maxes.2)
+        }) {
+            sum += id;
+        }
+    }
 
-    println!("Assembly found: {sum}");
+    println!("Sum is {sum}");
 }
 
-fn assembly2(input: &'static str) {
-    let sum: u64;
-    const WIN_TABLE: [u8; 3] = [0, 3, 6];
+fn part2(input: &str) {
+    let mut sum = 0;
 
-    unsafe {
-        asm! {
-            "mov {sum}, 0",
-            // Setup length
-            "shr {len}, 2",
-            "2:",
-            // Store 4 chars in {str}
-            "mov {arr:e}, [{str} + {len} * 4 - 4]",
-            // Mask space and newline
-            "sub {arr}, 0x00580041",
-            "mov {tmp}, 0x0000000000FF00FF",
-            "and {arr}, {tmp}",
-            // Use the sum to calc
-            // how much we get for our move.
-            // A X => 0 => 3
-            // B X => 1
-            // C X => 2
-            // A Y => 1
-            // B Y => 2
-            // C Y => 3
-            // A Z => 2
-            // B Z => 3
-            // C Z => 4 => 0
-            "mov {tmp}, 0",
-            "mov {tmp:l}, {arr:l}",
-            "shr {arr}, 16",
-            "add {tmp}, {arr}",
-            // Invert lowest case
-            "jnz 3f",
-            "add {tmp}, 3",
-            "3:",
-            // Invert Top Case
-            "and {tmp}, 3",
-            "jnz 3f",
-            "inc {tmp}",
-            "3:",
-            // Add to sum
-            "add {sum}, {tmp}",
-            // Add win bonus
-            "mov {tmp:l}, [{win} + {arr}]",
-            "and {tmp}, 0xFF",
-            "add {sum}, {tmp}",
-            // Loop
-            "dec {len}",
-            "jnz 2b",
-            // Holds the pointer to the string
-            str = inout(reg) input as *const str as *const u8 => _,
-            // Holds the 4 working characters of the string
-            arr = out(reg) _,
-            // Temp
-            tmp = out(reg) _,
-            // Win table for +3 +0 or +6
-            win = in(reg) &WIN_TABLE,
-            // The length of the string, also the index in the loop
-            len = inout(reg) input.len() => _,
-            // The total sum
-            sum = out(reg) sum,
-        };
-    };
+    for (_, rounds) in parse(input).unwrap() {
+        let (a, b, c) = rounds.into_iter().fold((0, 0, 0), |(d, e, f), (a, b, c)| {
+            (max(a, d), max(b, e), max(c, f))
+        });
+        sum += a * b * c;
+    }
 
-    println!("Assembly found: {sum}");
+    println!("Sum is {sum}");
 }
 
-fn part1(input: &'static str) {
-    let sum: i32 = input
-        .lines()
-        .map(|line| match line.chars().collect::<Vec<char>>()[..] {
-            ['A', ' ', 'X', ..] => 1 + 3,
-            ['A', ' ', 'Y', ..] => 2 + 6,
-            ['A', ' ', 'Z', ..] => 3 + 0,
-            ['B', ' ', 'X', ..] => 1 + 0,
-            ['B', ' ', 'Y', ..] => 2 + 3,
-            ['B', ' ', 'Z', ..] => 3 + 6,
-            ['C', ' ', 'X', ..] => 1 + 6,
-            ['C', ' ', 'Y', ..] => 2 + 0,
-            ['C', ' ', 'Z', ..] => 3 + 3,
-            ref chrs => {
-                println!("Error: {chrs:?}");
-                -10000
-            }
-        })
-        .sum();
-
-    println!("Final score: {sum}");
-}
-
-fn part2(input: &'static str) {
-    let sum: i32 = input
-        .lines()
-        .map(|line| match line.chars().collect::<Vec<char>>()[..] {
-            ['A', ' ', 'X', ..] => 0 + 3,
-            ['A', ' ', 'Y', ..] => 3 + 1,
-            ['A', ' ', 'Z', ..] => 6 + 2,
-            ['B', ' ', 'X', ..] => 0 + 1,
-            ['B', ' ', 'Y', ..] => 3 + 2,
-            ['B', ' ', 'Z', ..] => 6 + 3,
-            ['C', ' ', 'X', ..] => 0 + 2,
-            ['C', ' ', 'Y', ..] => 3 + 3,
-            ['C', ' ', 'Z', ..] => 6 + 1,
-            ref chrs => {
-                println!("Error: {chrs:?}");
-                -10000
-            }
-        })
-        .sum();
-
-    println!("Final score: {sum}");
+fn parse(input: &str) -> Result<Vec<(u32, Vec<(u32, u32, u32)>)>, Err<()>> {
+    //Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    separated_list0(
+        newline,
+        tuple((
+            preceded(tag("Game "), map_res(digit1, str::parse)), //
+            preceded(
+                tag(": "),
+                separated_list0(
+                    tag("; "),
+                    map(
+                        separated_list0(
+                            tag(", "),
+                            alt((
+                                terminated(
+                                    map(digit1, |num| (str::parse(num).unwrap(), 0, 0)),
+                                    tag(" green"),
+                                ),
+                                terminated(
+                                    map(digit1, |num| (0, str::parse(num).unwrap(), 0)),
+                                    tag(" blue"),
+                                ),
+                                terminated(
+                                    map(digit1, |num| (0, 0, str::parse(num).unwrap())),
+                                    tag(" red"),
+                                ),
+                            )),
+                        ),
+                        |l| {
+                            l.iter()
+                                .fold((0, 0, 0), |(a, b, c), (d, e, f)| (a + d, b + e, c + f))
+                        },
+                    ),
+                ),
+            ),
+        )),
+    )(input)
+    .map(|r| r.1)
 }
--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,211 +1,75 @@
-use itertools::Itertools;
-use std::arch::asm;
-
 fn main() {
     let input = include_str!("input.txt");
 
-    assembly1(input);
     part1(input);
-    assembly2(input);
     part2(input);
 }
 
-fn assembly1(input: &'static str) {
-    let mut sum = 0usize;
+fn part1(input: &str) {
+    let mut input: Vec<Vec<_>> = input.lines().map(|l| l.chars().collect()).collect();
+    let y_max = input.len() as isize;
+    let x_max = input[0].len() as isize;
 
-    unsafe {
-        asm! {
-            // Init
-            "dec {len}",
-            "mov {ind}, 0",
-            "2:",
-            // Stuff
-            "mov {line}, {ind}",
-            // Find \n
-            "3:",
-            "inc {line}",
-            "cmp byte ptr [{str} + {line}], '\n'",
-            "jnz 3b",
-            // Find middle
-            "sub {line}, {ind}",
-            "shr {line}, 1",
-            "mov {half}, {line}",
-            "add {line}, {ind}",
-            // Go through first half
-            "mov {temp}, 0",
-            "mov {char}, 0",
-            "4:",
-            "mov {temp:l}, [{str} + {ind}]",
-            "sub {temp}, 'A'",
-            "mov CL, {temp:l}",
-            "mov {temp}, 1",
-            "shl {temp}, CL",
-            "or {char}, {temp}",
-            "inc {ind}",
-            "cmp {line}, {ind}",
-            "jnz 4b",
-            "add {line}, {half}",
-            // Go through second half, find first match
-            "5:",
-            "mov {temp:l}, [{str} + {ind}]",
-            "sub {temp}, 'A'",
-            "mov CL, {temp:l}",
-            "mov {temp}, 1",
-            "shl {temp}, CL",
-            "inc {ind}",
-            "and {temp}, {char}",
-            "jz 5b",
-            // Get priority
-            "mov {temp:l}, [{str} + {ind} - 1]",
-            "cmp {temp:l}, 'Z' + 1",
-            "jns 6f",
-            // Handle upper case
-            "sub {temp:l}, 'A' - 27",
-            "jmp 7f",
-            // Handle lower case
-            "6:",
-            "sub {temp:l}, 'a' - 1",
-            // Add to sum
-            "7:",
-            "and {temp}, 0xFF",
-            "add {sum}, {temp}",
-            // For loop
-            "mov {ind}, {line}",
-            "inc {ind}",
-            "cmp {ind}, {len}",
-            "js 2b",
-            str = in(reg) input as *const str as *const u8,
-            len = in(reg) input.len(),
-            sum = inout(reg) sum,
-            ind = out(reg) _,
-            line = out(reg) _,
-            temp = out(reg) _,
-            char = out(reg) _,
-            half = out(reg) _,
-            out("cl") _,
-            options(pure, readonly, nostack),
+    let mut sum = 0;
+    for y in 0..y_max {
+        for x in 0..x_max {
+            if input[y as usize][x as usize] != '.' && !input[y as usize][x as usize].is_numeric() {
+                for yoff in -1..=1 {
+                    for xoff in -1..=1 {
+                        sum += find_num(x + xoff, y + yoff, &mut input)
+                            .parse::<u32>()
+                            .unwrap_or(0);
+                    }
+                }
+            }
         }
-    };
+    }
 
-    dbg!(sum);
+    println!("Sum: {sum}");
 }
 
-fn part1(input: &'static str) {
-    let sum: u32 = input
-        .lines()
-        .map(|l| l.split_at(l.len() / 2))
-        .map(|(f, b)| f.chars().filter(|c| b.chars().contains(c)).next().unwrap() as u32)
-        .map(|c| match c as u8 {
-            b'a'..=b'z' => c - 'a' as u32 + 1,
-            b'A'..=b'Z' => c - 'A' as u32 + 27,
-            _ => panic!("Not supposed to be here..."),
-        })
-        .sum();
+fn part2(input: &str) {
+    let mut input: Vec<Vec<_>> = input.lines().map(|l| l.chars().collect()).collect();
+    let y_max = input.len() as isize;
+    let x_max = input[0].len() as isize;
 
-    println!("Sum is {sum}");
-}
+    let mut sum = 0;
+    for y in 0..y_max {
+        for x in 0..x_max {
+            if input[y as usize][x as usize] == '*' {
+                let mut count = 0;
+                let mut mult = 1;
 
-fn assembly2(input: &'static str) {
-    let mut sum = 0usize;
+                for yoff in -1..=1 {
+                    for xoff in -1..=1 {
+                        if let Ok(num) = find_num(x + xoff, y + yoff, &mut input).parse::<u32>() {
+                            mult *= num;
+                            count += 1;
+                        }
+                    }
+                }
 
-    unsafe {
-        asm! {
-            // Init
-            "dec {len}",
-            "mov {ind}, 0",
-            // Go through first line
-            "2:",
-            "mov {temp}, 0",
-            "mov {first}, 0",
-            "mov {final}, 0",
-            "3:",
-            "mov {temp:l}, [{str} + {ind}]",
-            "cmp {temp:l}, '\n'",
-            "jz 4f",
-            "sub {temp}, 'A'",
-            "mov CL, {temp:l}",
-            "mov {temp}, 1",
-            "shl {temp}, CL",
-            "or {first}, {temp}",
-            "inc {ind}",
-            "jmp 3b",
-            // Go through second line, store all matchs
-            "4:",
-            "inc {ind}",
-            "mov {temp:l}, [{str} + {ind}]",
-            "cmp {temp:l}, '\n'",
-            "jz 5f",
-            "sub {temp}, 'A'",
-            "mov CL, {temp:l}",
-            "mov {temp}, 1",
-            "shl {temp}, CL",
-            "and {temp}, {first}",
-            "or {final}, {temp}",
-            "jmp 4b",
-            // Go through third line, find first match
-            "5:",
-            "inc {ind}",
-            "mov {temp:l}, [{str} + {ind}]",
-            "sub {temp}, 'A'",
-            "mov CL, {temp:l}",
-            "mov {temp}, 1",
-            "shl {temp}, CL",
-            "and {temp}, {final}",
-            "jz 5b",
-            // Get priority
-            "mov {temp:l}, [{str} + {ind}]",
-            "cmp {temp:l}, 'Z' + 1",
-            "jns 6f",
-            // Handle upper case
-            "sub {temp:l}, 'A' - 27",
-            "jmp 7f",
-            // Handle lower case
-            "6:",
-            "sub {temp:l}, 'a' - 1",
-            // Add to sum
-            "7:",
-            "and {temp}, 0xFF",
-            "add {sum}, {temp}",
-            // Find end of line
-            "8:",
-            "inc {ind}",
-            "cmp byte ptr [{str} + {ind}], '\n'",
-            "jnz 8b",
-            "inc {ind}",
-            // For loop
-            "cmp {ind}, {len}",
-            "js 2b",
-            str = in(reg) input as *const str as *const u8,
-            len = in(reg) input.len(),
-            sum = inout(reg) sum,
-            ind = out(reg) _,
-            temp = out(reg) _,
-            first = out(reg) _,
-            final = out(reg) _,
-            out("cl") _,
-            options(pure, readonly, nostack),
+                if count == 2 {
+                    sum += mult;
+                }
+            }
         }
-    };
+    }
 
-    dbg!(sum);
+    println!("Gear Ratio Sum: {sum}");
 }
 
-fn part2(input: &'static str) {
-    let sum: u32 = input
-        .lines()
-        .tuples()
-        .map(|(a, b, c)| {
-            a.chars()
-                .filter(|d| b.chars().contains(d) && c.chars().contains(d))
-                .next()
-                .unwrap() as u32
-        })
-        .map(|c| match c as u8 {
-            b'a'..=b'z' => c - 'a' as u32 + 1,
-            b'A'..=b'Z' => c - 'A' as u32 + 27,
-            _ => panic!("Not supposed to be here..."),
-        })
-        .sum();
+fn find_num(x: isize, y: isize, input: &mut Vec<Vec<char>>) -> String {
+    if x < 0
+        || y < 0
+        || y >= input.len() as isize
+        || x >= input[y as usize].len() as isize
+        || !input[y as usize][x as usize].is_numeric()
+    {
+        return "".to_string();
+    }
 
-    println!("Sum is {sum}");
+    let string = input[y as usize][x as usize].to_string();
+    input[y as usize][x as usize] = '.';
+    find_num(x - 1, y, input) + &string + &find_num(x + 1, y, input)
 }
